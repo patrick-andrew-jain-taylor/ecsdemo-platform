bed9e5aae86ab59dfba5a98ff9524c93c6052f29
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-06-09 12:40:00 -0400

    Removing service-linked role.

--- aws_iam_service_linked_role.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_iam_service_linked_role" "AWSServiceRoleForElasticLoadBalancing" {
-  aws_service_name = "elasticloadbalancing.amazonaws.com"
-}
-
-resource "aws_iam_service_linked_role" "AWSServiceRoleForECS" {
-  aws_service_name = "ecs.amazonaws.com"
-}
